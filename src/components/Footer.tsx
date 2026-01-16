3213d45f0317799f50477d4803080779a15b0337
Thiago Medeiros <thiagotitico1@gmail.com>
2026-01-16 12:08:12 -0300

    footer estilizado

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,25 @@
 import './styles/Footer.css'
+import logo from '/logo.svg'
 
 export default function Footer() {
-    return(
-        <h1>teste</h1>
-    )
+    return(<footer className='Footer'>
+        <div className='Footer_itens'>
+            <div className='Footer_itens_informacoes'>
+                <div className='Footer_itens_informacoes_lista'>
+                    <h5>Páginas</h5>
+                    <div className='Footer_itens_informacoes_lista_itens'>
+                        <a href="">Início</a>
+                        <a href="/sobre">Sobre</a>
+                    </div>
+                </div>
+                <div className='Footer_itens_informacoes_lista'>
+                    <h5>Informações</h5>
+                    <div className='Footer_itens_informacoes_lista_itens'>
+                        <a href="">Repositório</a>
+                    </div>
+                </div>
+            </div>
+            <img src={logo} alt="Logo do site" />
+        </div>
+    </footer>)
 }
