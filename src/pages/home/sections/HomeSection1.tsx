d75eb8d1898ea0a8fa1dd45c3c1fa1e6610adb77
Thiago Medeiros <thiagotitico1@gmail.com>
2026-01-16 21:20:28 -0300

    Pronto pra teste

--- src/pages/home/sections/HomeSection1.tsx
+++ src/pages/home/sections/HomeSection1.tsx
@@ -1,6 +1,49 @@
 import './styles/HomeSection1.css'
+import { useState } from 'react';
+
+type VerificacaoEmailResponse = {
+    classificacao_do_email: string;
+    justificativa: string;
+    sugestao: string;
+};
 
 export default function HomeSection1() {
+    const [classificacaoDoEmail, setClassificacaoDoEmail] = useState("Aguardando Email");
+    // const [justificativa, setJustificativa] = useState("");
+    const [sugestao, setSugestao] = useState("");
+    const [email, setEmail] = useState<string>("");
+    const [respostaApi, setRespostaApi] = useState("");
+
+    async function verificarEmail(email : string) {
+        setRespostaApi("aguardando");
+
+        const response = await fetch(
+            "https://web-production-bed1.up.railway.app/verificar_email",
+            {
+                method: "POST",
+                headers: {
+                "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ 'email' : email }),
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error('Erro na requisição');
+        }
+
+        const data: VerificacaoEmailResponse = await response.json();
+
+        console.log(data.classificacao_do_email);
+        setClassificacaoDoEmail(data.classificacao_do_email);
+        console.log(data.justificativa);
+        // setJustificativa(data.justificativa);
+        console.log(data.sugestao);
+        setSugestao(data.sugestao);
+
+        setRespostaApi("chegou");
+    }
+
     return(
     <section className="HomeSection1">
         <div className='HomeSection1_texto'>
@@ -16,17 +59,31 @@ export default function HomeSection1() {
         <div className='HomeSection1_inputs'>
             <div className='HomeSection1_inputs_itens'>
                 <h3>Digite abaixo o email</h3>
-                <textarea placeholder="Escreva aqui..."></textarea>
+                <textarea
+                    placeholder="Escreva aqui..."
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <button className='button_enviar_email' onClick={() => verificarEmail(email)}>Clique aqui para enviar o email para analise!</button>
+
+                <div className={`mensagem_email_sendo_verificado ${respostaApi === "aguardando" ? "ativo" : ""}`}>
+                    <h3>Aguarde!</h3>
+                    <h4>Seu email está sendo analisado pelo nosso sistema</h4>
+                </div>
+
+                <div className={`mensagem_email_foi_verificado ${respostaApi === "chegou" ? "ativo" : ""}`}>
+                    <h3>Sucesso!</h3>
+                    <a href="#sugestao"><h4>Clique <strong>aqui</strong> e veja a sugestão de resposta</h4></a>
+                </div>
             </div>
-            <div className='HomeSection1_inputs_itens_classificacao'>
+            <div className='HomeSection1_inputs_itens_classificacao' id='sugestao'>
                 <h3>Classificação do email</h3>
-                <h5>Aguardando Email</h5>
+                <h5>{classificacaoDoEmail}</h5>
             </div>
             <div className='HomeSection1_inputs_itens'>
                 <h3>Sugestão de resposta</h3>
                 <div className='HomeSection1_inputs_itens_sugestao_resposta'>
-                    {/* <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p> */}
-                    <p></p>
+                    <p>{sugestao}</p>
                     <button>Clique aqui, e copia a resposta</button>
                 </div>
             </div>
